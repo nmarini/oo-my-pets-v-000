a0d80989d084c951f0e11e1751f35a28f5accad4
Nicolas Marini <nmarini44@gmail.com>
2018-06-26 00:21:48 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,10 +1,10 @@
 class Fish
   # code goes here
-  attr_accessor :mood 
-  attr_reader :name 
+  attr_accessor :mood
+  attr_reader :name
 
-  def initialize(name) 
-    @name = nme 
+  def initialize(name)
+    @name = name
     @mood = "nervous"
   end
 end
