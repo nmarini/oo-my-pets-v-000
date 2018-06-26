3682d3dbb5fe7d083885234fe2a8976023a42a30
Nicolas Marini <nmarini44@gmail.com>
2018-06-26 00:21:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   # code goes here
   attr_accessor :mood
-  attr_reader :name 
+  attr_reader :name
 
   def initialize(name)
-    @name = name 
+    @name = name
     @mood = "nervous"
   end
 end
